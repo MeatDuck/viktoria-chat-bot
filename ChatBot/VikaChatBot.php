583a18ca6c70f9a62515507f756bf465569ee1c4
aahomchenko@gmail.com <aahomchenko@gmail.com@40ee8e45-28c1-5245-5191-b58d98fafcb9>
2012-02-21 19:12:24 +0000

    initial commit

---
+++ ChatBot/VikaChatBot.php
@@ -0,0 +1,70 @@
+﻿<?php
+/**
+ * Settings
+ */
+
+include_once 'AbstractChat.php';
+class VikaChatBot extends AbstractChat {
+    /**
+     * @var Memcache
+     */
+    private $cache;
+    /**
+     * @var VikaChatBot
+     */
+    private static $instance = null;
+
+    private function VikaChatBot(){
+        $this->cache = new Memcache();
+        $this->cache->connect("127.0.0.1", "11211");
+    }
+
+    /**
+     * @return VikaChatBot
+     */
+    public static function getInstance(){
+        if(self::$instance == null)
+                self::$instance = new VikaChatBot();
+        return self::$instance;
+    }
+
+    /**
+     * @return Boolean
+     */
+    public function isEnabled(){
+        if($this->cache == null)
+        return false;
+        if($this->cache->get("isChatBot") === null){
+        	$this->cache->set("isChatBot", false);
+        }
+        if($this->cache->get("isChatBot") === true){
+        	return TRUE;
+        }
+        
+        return FALSE;
+    }
+ 
+    public function setupUI(){
+        $this->cache->set("isChatBot", true);
+    }
+    
+    public function offUI(){
+        $this->cache->set("isChatBot", false);
+    }
+    
+    public function say($sentence){
+    	$sentence = parent::say($sentence);
+    	$mapper = new AnswerMapper($this->cache);
+    	$sentence = $mapper->say($sentence)
+    					   ->VariantLoad()
+    					   ->SituationLoad()
+    	                   ->SearchLoadTab()
+    					   ->SearchResLoad()
+    					   ->EmptyQuestionLoad()    	                   
+    					   ->Load();
+    	return mb_convert_encoding($sentence, "Windows-1251");
+    }
+}
+
+
+
